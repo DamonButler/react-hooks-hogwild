d7b179c1456dfe216e0345c77bcfd9537b80aecd
Mason Parks <kmparks60@gmail.com>
2023-02-24 03:23:10 -0600

    oink oink

--- src/components/HogTiles.js
+++ src/components/HogTiles.js
@@ -1,9 +1,19 @@
-function HogTile({ hog })
-{
+import { useState } from "react";
+
+function HogTile({ hog }) {
+    const [showingDetails, setShowingDetails] = useState (false)
     return (
         <div>
             <h2>{hog.name}</h2>
             <img src={hog.image} alt={hog.name} />
+            {showingDetails ? (
+                <div>
+                    <p>speciality: {hog.specialty}</p>
+                    <p>greased: {hog.greased ? "yes" : "no"}</p>
+                    <p>weight: {hog.weight}</p>
+                    <p>medal: {hog["highest medal achieved"]}</p>
+                </div>
+            ) : null }
         </div>
     );
 }
